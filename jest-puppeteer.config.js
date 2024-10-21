a120da069b30c254601475976bf801927d0eebf9
annyong1 <fbcjeremy@gmail.com>
2024-10-21 19:20:57 -0400

    4.6 fix2

--- jest-puppeteer.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  launch: {
-    headless: true, // set to false if you want to see the browser during testing
-    product: "chrome", // specify product if needed, e.g. chrome
-  },
-  browserContext: "default",
-};
