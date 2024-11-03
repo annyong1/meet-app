007c2d172356f6469780b681c65470c0eeed5033
annyong1 <fbcjeremy@gmail.com>
2024-11-03 13:00:07 -0500

    4.9.3

--- src/api.js
+++ src/api.js
@@ -41,8 +41,17 @@ const checkToken = async (accessToken) => {
  * This function will fetch the list of all events
  */
 export const getEvents = async () => {
-  if (window.location.href.startsWith("http://localhost")) {
-    return mockData;
+  const response = await fetch(url);
+  const result = await response.json();
+  if (result) {
+    NProgress.done();
+    localStorage.setItem("lastEvents", JSON.stringify(result.events));
+    return result.events;
+  } else return null;
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
   }
 
   const token = await getAccessToken();
