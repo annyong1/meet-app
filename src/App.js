a01c3bf59f0a5ee0b744664bb6f97f866162f457
annyong1 <fbcjeremy@gmail.com>
2024-11-03 19:25:07 -0500

    4.9.7

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CitySearch from "./components/CitySearch";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert} from "./components/Alert";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -13,8 +13,14 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (!navigator.onLine) {
+      setWarningAlert("You are offline. Events data may be outdated.")
+    } else {
+      setWarningAlert("");
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -46,6 +46,14 @@ class InfoAlert extends Alert {
       this.color = 'rgb(255, 0, 0)'; // red
       this.bgColor = 'rgb(255, 127, 127)'; // light red
     }
-}
+  }
 
-export { InfoAlert, ErrorAlert };
+  class WarningAlert extends Alert { 
+    constructor(props) {
+      super(props);
+      this.color = "rgb(150, 174, 55)";
+      this.bgColor = "rgb(255, 255, 220)";
+    }
+  }
+
+export { InfoAlert, ErrorAlert, WarningAlert };
