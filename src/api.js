c48f9ddd0671a00148e181f7baed5388d63dcd52
annyong1 <fbcjeremy@gmail.com>
2024-11-03 20:24:20 -0500

    4.9.11

--- src/api.js
+++ src/api.js
@@ -71,6 +71,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
